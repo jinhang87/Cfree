b3d37ef9ba65b34344e588ab9c86d8db54f051ba
jinhang87 <jinhang87@163.com>
2017-05-04 21:19:47 +0800

    [chg] c++11-example: add producer-comsumer; key-multivalue map ;

---
+++ c++11-example/example_producer-consumer.cpp
@@ -0,0 +1,71 @@
+/*
+	生产者消费者问题 
+*/
+#include <iostream>           
+#include <queue>
+#include <thread>             
+#include <mutex>              
+#include <condition_variable> 
+
+using namespace std;
+
+mutex mtx;
+condition_variable isNotFull, isNotEmpty;
+
+queue<int> q;     // shared value by producers and consumers, which is the critical section
+int maxSize = 20;
+
+void consumer() 
+{
+    while (true)
+    {
+        this_thread::sleep_for(chrono::milliseconds(1000));
+
+        unique_lock<mutex> lck(mtx);                        // RAII，程序运行到此block的外面（进入下一个while循环之前），资源（内存）自动释放
+        isNotEmpty.wait(lck, [] {return q.size() != 0; });     // wait(block) consumer when q.size() == 0
+
+        cout << "consumer " << this_thread::get_id() << ": ";
+        q.pop();
+        cout << q.size() << '\n';
+
+        isNotFull.notify_all();                               // nodity(wake up) producer when q.size() != maxSize
+    }
+}
+
+void producer(int id)
+{
+    while (true)
+    {
+        this_thread::sleep_for(chrono::milliseconds(900));      // producer is a little faster than consumer  
+
+        unique_lock<mutex> lck(mtx);
+        isNotFull.wait(lck, [] {return q.size() != maxSize; });   // wait(block) producer when q.size() == maxSize 
+
+        cout << "-> producer " << this_thread::get_id() << ": ";
+        q.push(id);
+        cout << q.size() << '\n';
+
+        isNotEmpty.notify_all();                                   // notify(wake up) consumer when q.size() != 0    
+    }
+}
+
+int example_producer_consumer()
+{
+    thread consumers[2], producers[2];
+
+    // spawn 2 consumers and 2 producers:
+    for (int i = 0; i < 2; ++i)
+    {
+        consumers[i] = thread(consumer);
+        producers[i] = thread(producer, i + 1);
+    }
+
+    // join them back: (in this program, never join...)
+    for (int i = 0; i < 2; ++i)
+    {
+        producers[i].join();
+        consumers[i].join();
+    }
+
+    system("pause");
+}
--- c++11-example/main.cpp
+++ c++11-example/main.cpp
@@ -1,11 +1,52 @@
-#include <iostream>
+#include <iostream>	//std::cout
+#include <map> //std::map
+#include <unordered_map> //std::unordered_map
+#include <string> //std::string
+#include <functional> //std::function
+#include <tuple>// std::tuple
 
 using namespace std;
 extern void example_has_member();
+extern void example_producer_consumer();
+
+typedef std::tuple<std::string, std::string, std::function<void(void)>> TUPLE_FUCTIONS;
+typedef std::map<std::string, TUPLE_FUCTIONS> MAP_FUCTIONS;
+MAP_FUCTIONS map_functions = 
+{
+	{ "a", std::make_tuple("has member", "use to know whether class has member", example_has_member) }, 
+	{ "b", std::make_tuple("producer_consumer", "Consumer and producer problem", example_producer_consumer) }, 
+};
+
+//一键多值应用 
+
+void help()
+{
+	auto i = 0;
+	for(auto it : map_functions)
+	{
+		cout << it.first << ": " <<std::get<0>(it.second) << "\t";
+		cout << std::get<1>(it.second) << endl;
+	}	
+}
 
 int main(int argc, char *argv[])
 {
-	example_has_member();
-	cout<<"Hello C-Free!"<<endl;
+	help();
+	std::string strcin; 
+	cout<<endl;
+	cout<<endl;
+	cout<<"Please Input Number>> ";
+	cin>>strcin;
+	
+	MAP_FUCTIONS::iterator it = map_functions.find(strcin); 
+	if(it != map_functions.end())
+	{
+		cout << std::get<0>(it->second) << " start :" << endl;
+		std::get<2>(it->second)();
+		
+	}
+	
+	//cout<<map_functions[0].first<<endl; 
+	cout<<"Hello C-Free!"<< strcin <<endl;
 	return 0;
 }
