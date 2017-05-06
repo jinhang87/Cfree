a8f9e0f0193b1ba8e8123d651cdff6dc23581ac5
jinhang87 <jinhang87@163.com>
2017-05-06 21:57:43 +0800

    [chg] thread +native_handle +yield

--- c++11.thread/main.cpp
+++ c++11.thread/main.cpp
@@ -6,11 +6,15 @@ using namespace std;
 
 extern int test_basic();
 extern int test_producer_consumer();
+extern void test_native_handle();
+extern void test_yield();
 
 int main(int argc, char *argv[])
 {
 	//test_basic();
-	test_producer_consumer();
+	//test_producer_consumer();
+	test_native_handle();
+	//test_yield();
 	cout<<"Hello C-Free!"<<endl;
 	return 0;
 }
